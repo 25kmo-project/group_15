4cc1e84dc7355272ba8d215b52733e027d5ac0f3
Mariia <mariia@Mariias-MacBook-Air.local>
2026-01-21 10:43:23 +0200

    päivitetty app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//luodaan uusi muuttuja ja tallennetaan sinne user.js
+var userRouter = require('./routes/user');
 
 var app = express();
 
@@ -15,6 +16,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//kaikki pyynnöt jotka alkaavat /user käsittelee userRouter
+app.use('/user', userRouter);
 
+//tämä antaa mahdollisuus käyttää tätä tiedostoa muissa tiedostoissa
 module.exports = app;
