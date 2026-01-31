d1e3ea5dc474677550e812d960731d53062109bc
Mariia <mariia@Mariias-MacBook-Air.local>
2026-01-31 13:30:22 +0200

    muokkauksia bcrypt pinkoodille

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ var transactionRouter = require('./routes/transaction');
 var cardRouter = require('./routes/card');
 var accountAccessRouter = require('./routes/account_access');
 var accountRouter = require('./routes/account'); // lisätty account-reititin
+//login
+var loginRouter = require('./routes/login');
+const jwt = require('jsonwebtoken');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -18,11 +22,33 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter); 
+
+//suojatut reitit
+//esimerkki
+// app.use('/user',authenticateToken, userRouter);
 app.use('/user', userRouter);
 app.use('/transaction', transactionRouter);
 app.use('/cards', cardRouter);
 app.use('/account_access', accountAccessRouter);
 app.use('/account', accountRouter); // määritellään /account-reititin
+
+
+//tarkisus jwt(jason web token)
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    //Extract token by removing "Bearer" prefix
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.MY_TOKEN, function(err,payload){
+        if(err){
+            return res.sendStatus(403);
+        }
+        req.card_id=payload.card_id;
+        next();
+    }) 
+}
 
 //tämä antaa mahdollisuus käyttää tätä tiedostoa muissa tiedostoissa
 module.exports = app;
