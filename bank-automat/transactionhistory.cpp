f13cbbb608f207f5d9e392b910d97e7c6333a2bd
Mariia <mariia@Mariias-MacBook-Air.local>
2026-02-13 10:59:06 +0200

    small fix in window settings

--- bank-automat/transactionhistory.cpp
+++ bank-automat/transactionhistory.cpp
@@ -4,10 +4,10 @@
 #include <QHeaderView>
 #include <QDebug>
 #include <QUrlQuery>
-#include <QNetworkRequest> 
+#include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QJsonDocument>
-#include <QJsonArray> 
+#include <QJsonArray>
 #include <QJsonObject>
 
 TransactionHistory::TransactionHistory(int accId, int cId, QString t, QWidget *parent) :
@@ -16,12 +16,14 @@ TransactionHistory::TransactionHistory(int accId, int cId, QString t, QWidget *p
 {
     ui->setupUi(this);
     manager = new QNetworkAccessManager(this);
+
     if (!ui->tableTransactions) {
-         qDebug() << "Table widget not found in UI!";
-         return; 
+        qDebug() << "ERROR: Table widget not found in UI!";
+        return;
     }
+
     initTableStyle();
-    displayData(); 
+    displayData();
 }
 
 TransactionHistory::~TransactionHistory()
@@ -33,26 +35,33 @@ void TransactionHistory::initTableStyle()
 {
     QHeaderView *header = ui->tableTransactions->horizontalHeader();
     header->setSectionResizeMode(QHeaderView::Interactive);
-    
+
     header->setSectionResizeMode(0, QHeaderView::Stretch);
-    
+
     header->setSectionResizeMode(1, QHeaderView::Fixed);
-    ui->tableTransactions->setColumnWidth(1, 100); // Type
-    
+    ui->tableTransactions->setColumnWidth(1, 100);
+
     header->setSectionResizeMode(2, QHeaderView::Fixed);
-    ui->tableTransactions->setColumnWidth(2, 100); // Amount
-    
+    ui->tableTransactions->setColumnWidth(2, 100);
+
     header->setSectionResizeMode(3, QHeaderView::Fixed);
-    ui->tableTransactions->setColumnWidth(3, 60);  // ID
+    ui->tableTransactions->setColumnWidth(3, 60);
 
     ui->tableTransactions->verticalHeader()->setVisible(false);
 }
 
 void TransactionHistory::displayData()
 {
+    if (!ui->lblPageNumber) {
+        qDebug() << "ERROR: lblPageNumber is NULL!";
+        return;
+    }
+
     ui->lblPageNumber->setText(QString("Page: %1").arg(currentPage));
+
     QString path = "transaction/history";
     QUrl url(Environment::base_url() + path);
+
     QUrlQuery query;
     query.addQueryItem("account_id", QString::number(accountId));
     query.addQueryItem("card_id", QString::number(cardId));
@@ -64,32 +73,30 @@ void TransactionHistory::displayData()
 
     QNetworkReply *reply = manager->get(request);
 
-    // connect the reply's finished signal to a lambda function to handle the response
     connect(reply, &QNetworkReply::finished, [this, reply]() {
         if (reply->error() == QNetworkReply::NoError) {
             QByteArray response = reply->readAll();
             QJsonDocument doc = QJsonDocument::fromJson(response);
             QJsonArray jsonArray = doc.array();
 
-            // new page,clear old data
             ui->tableTransactions->setRowCount(0);
 
-            if (jsonArray.size() < 10) {
-                ui->btnNextPage->setEnabled(false);
-            } else {
-                ui->btnNextPage->setEnabled(true);
+            if (!ui->btnNextPage || !ui->btnPrevPage) {
+                qDebug() << "ERROR: Navigation buttons not found!";
+                reply->deleteLater();
+                return;
             }
 
-            // if page is 1,disable previous page button
+            ui->btnNextPage->setEnabled(jsonArray.size() >= 10);
             ui->btnPrevPage->setEnabled(currentPage > 1);
 
-            // 4. populate the table with data
             for (const QJsonValue &value : jsonArray) {
                 QJsonObject obj = value.toObject();
                 int row = ui->tableTransactions->rowCount();
                 ui->tableTransactions->insertRow(row);
 
                 ui->tableTransactions->setItem(row, 0, new QTableWidgetItem(obj["Aika"].toString()));
+
                 QTableWidgetItem *typeItem = new QTableWidgetItem(obj["Tyyppi"].toString());
                 typeItem->setTextAlignment(Qt::AlignCenter);
                 ui->tableTransactions->setItem(row, 1, typeItem);
@@ -104,21 +111,19 @@ void TransactionHistory::displayData()
                 ui->tableTransactions->setItem(row, 3, idItem);
             }
         } else {
-            qDebug() << "Error fetching data:" << reply->errorString();
+            qDebug() << "Error fetching transaction history:" << reply->errorString();
             ui->btnNextPage->setEnabled(false);
         }
         reply->deleteLater();
     });
 }
 
-// next page
 void TransactionHistory::on_btnNextPage_clicked()
 {
     currentPage++;
-    displayData(); 
+    displayData();
 }
 
-// previous page
 void TransactionHistory::on_btnPrevPage_clicked()
 {
     if(currentPage > 1) {
@@ -127,8 +132,7 @@ void TransactionHistory::on_btnPrevPage_clicked()
     }
 }
 
-// back to menu
 void TransactionHistory::on_btnBackToMenu_clicked()
 {
-    this->close(); 
+    this->close();
 }
