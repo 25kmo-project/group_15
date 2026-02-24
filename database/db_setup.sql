2d9bd59e6b8a91c94ca2ef772591e7abaf129b16
Mariia <mariia@Mariias-MacBook-Air.local>
2026-02-24 14:53:35 +0200

    admin privileges have changed

--- database/db_setup.sql
+++ database/db_setup.sql
@@ -2,6 +2,6 @@ CREATE DATABASE IF NOT EXISTS bank;
 
 CREATE USER 'bank_admin'@'localhost' IDENTIFIED BY 'YOUR_PASSWORD_HERE';
 
-GRANT ALL PRIVILEGES ON `bank`.* TO 'bank_admin'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON bank.* TO 'bank_admin'@'localhost';
 
 FLUSH PRIVILEGES;
